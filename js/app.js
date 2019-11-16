c0b959e41d86b5cbae12171a5905b26248778572
Jatinokov <Enkhtaivan1206@gmail.com>
2019-11-16 02:00:37 -0800

    added JS functions for scroll menu, styled in css

--- js/app.js
+++ js/app.js
@@ -1,3 +1,37 @@
-// $(".welcome").click(function(){
-//     $(this).parent().fadeOut(500);
-// });
+$(document).ready(function(){
+    // var scrollTop = $(".scrolltop");
+
+    var main = $(".main").position();
+    var top = $(".top").position();
+    var text = $(".text").position();
+    var cont = $(".cont").position();
+
+    $("#about").click(function(){
+
+        $("html, body").animate({scrollTop: main.top}, 1000);
+
+            return false;
+    });
+
+    $("#portfolio").click(function(){
+        $("html, body").animate({scrollTop: top.top}, 1000);
+        return false;
+
+    });
+
+    $("#service").click(function(){
+        $("html, body").animate({scrollTop: text.top}, 1000);
+        return false;
+
+    });
+    
+    $("#contac").click(function(){
+        $("html, body").animate({scrollTop: cont.top}, 1000);
+        return false;
+    })
+
+
+
+
+    });
+  
